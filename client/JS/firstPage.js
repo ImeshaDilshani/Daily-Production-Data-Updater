1428c8f86ae3499076b4d907db4632ea9bc19bd6
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-07-26 22:14:05 +0530

    local storage updated

--- client/JS/firstPage.js
+++ client/JS/firstPage.js
@@ -1,18 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-
     const goToProfileCard = document.getElementById('goToProfile');
-    const selectOtherProfileCard = document.getElementById('selectOtherProfile');
 
     goToProfileCard.addEventListener('click', () => {
-
-        const profileData = localStorage.getItem('profileData');
-        // what is profileData?--------------------------------------------------------------------------------------------------
-        // Assuming you have a function to save the selected profile data
-        function saveProfileData(profileData) {
-            localStorage.setItem('profileData', JSON.stringify(profileData));
-            window.location.href = 'thirdPage.html';
-        }
-
+        const profileData = localStorage.getItem('selectedEmployee');
+        
         if (profileData) {
             // If profile data is already saved in local storage, go to the third page
             window.location.href = 'thirdPage.html';
@@ -20,10 +11,5 @@ document.addEventListener('DOMContentLoaded', () => {
             // If no profile data, go to the second page to select profile
             window.location.href = 'secondPage.html';
         }
-    });
-
-    selectOtherProfileCard.addEventListener('click', () => {
-        // Always go to the second page to select profile
-        window.location.href = 'secondPage.html';
     });
 });
